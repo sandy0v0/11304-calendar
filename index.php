7520bc27513c55ef43c7a2fd00d171baceac7772
sandy0v0 <be0324ss@gmail.com>
2024-11-12 17:05:57 +0800

    新增hover效果

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
   <title>萬年曆作業</title>
   <style>
    /*請在這裹撰寫你的CSS*/ 
-   body {
+    body {
     text-align: center ; /* 設定容器的樣式，使按鈕居中顯示 */
-    background-image: url('../11304-calendar-sandy0v0/images/background.PNG'); /*設定背景圖片*/
-    /* background-size: cover; 使圖片覆蓋整個背景 */
+    background-image: url('./images/5.jfif'); /*設定背景圖片*/
+    background-size: cover;/*使圖片覆蓋整個背景*/
     background-position: center; /* 圖片置中 */
     background-repeat: no-repeat; /* 不重複顯示背景圖片 */
     background-attachment: fixed; /* 固定背景圖不隨捲動 */
@@ -20,7 +20,7 @@
     /* 設定背景上文字的顏色和樣式，以確保在背景圖上清晰可見 */
     h1 {
         color: lightcoral;
-        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); /* 添加陰影提高可讀性 */
+        text-shadow: 1px 2px 1px rgba(0, 0, 0, 0.5); /* 添加陰影提高可讀性 */
     }
 
     table {
@@ -29,7 +29,7 @@
         /* border-collapse:collapse; 使邊框合併 */
         margin:auto;        
         /* background: rgb(<?rand(50,250);?>,<?rand(50,250);?>,<?rand(50,250);?>); */
-        background-color: rgba(255, 255, 255, 0.9); /* 增加日曆的白色半透明背景 */
+        background-color: rgba(255, 255, 255, 0.8); /* 增加日曆的白色半透明背景 */
         border-radius: 8px;
     }
 
@@ -41,6 +41,7 @@
     td {
         width: 100px;
         height: 50px;
+        font-weight: bold;
         padding: 10px 10px; /* 單元格內的邊距 */
         text-align:center; /* 文字居中 */
         border:2px solid #999; /* 邊框顏色 */
@@ -49,22 +50,23 @@
     }
     .holiday {
         background: pink; /* 假日的背景顏色 */
-        color: red; /* 假日的文字顏色 */
-    }
+        color: red; /* 假日的文字顏色 */      
+      }
     .grey-text {
         color: #999; /* 非當月日期的文字顏色 */
     }
     .today {
-        background: blue; /* 今天的背景顏色 */
+        background: lightblue; /* 今天的背景顏色 */
         color: white; /* 今天的文字顏色 */
         font-weight:bolder; /* 加粗字體 */
+        text-shadow: 1px 2px 1px rgba(0, 0, 0, 0.5);
     }
     .nav{
         width: 700px;
         margin:auto;
-        background-color: rgba(255, 255, 255, 0.8); /* 加入白色半透明背景以強調選單 */
+        background-color: rgba(255, 255, 255, 0.5); /* 加入白色半透明背景以強調選單 */
         border-radius: 8px;
-        padding: 10px;
+        padding: 2px;
     }
 
     .nav td{
@@ -72,7 +74,6 @@
         border:10px;
         padding:10px 10px;
         text-decoration: none;
-
     }
 
     .today-button {
@@ -80,23 +81,28 @@
     text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
     }
 
-    /* 設定按鈕的樣式 */
+      /* 設定按鈕的樣式 */
     .today-link {
-    padding: 10px 330px 10px; /* 按鈕的內邊距 */
-    background-color: lightblue; /* 按鈕的背景顏色 */
+    padding: 8px 40px 8px; /* 按鈕的內邊距 */
+    background-color: rgba(255, 199, 95, 0.8); 
+    /*background: rgba(255, 150, 113, 0.5); 按鈕的背景顏色 */
     color: white; /* 按鈕的文字顏色 */
     text-decoration: none; /* 取消超鏈接的下劃線 */
     border-radius: 20px; /* 按鈕的圓角效果 */
     }
 
-    /* 設定按鈕的樣式 */
-    .today-link1 {
-    padding: 10px 45px 10px; /* 按鈕的內邊距 */
-    /* background-color: lightblue;  */
-    background: rgba(255, 150, 113, 0.5);
-    color: white; /* 按鈕的文字顏色 */
-    text-decoration: none; /* 取消超鏈接的下劃線 */
-    border-radius: 20px; /* 按鈕的圓角效果 */
+    .today-link:hover {
+    background-color: lightcoral;
+    }
+
+    a {
+      text-decoration: none;
+      font-weight:bolder;
+      color: #98D98E;
+    }
+
+    .nextMonth:hover {
+    color: rgba(255, 199, 95, 0.8);
     }
 
     .sp-date {
@@ -115,7 +121,8 @@
     /* 調整 << 和 >> 的樣式 */
     .nextYear {
         color: #fff;
-        background: rgba(255, 199, 95, 0.8);
+        background:lightcoral;
+        /* background: rgba(255, 199, 95, 0.8); */
         border-radius: 50%; /* 設置為圓框 */
         font-size: 24px; /* 調整大小 */
         width: 50px;
@@ -134,12 +141,17 @@
         text-decoration: none;
     }
 
+    .nextYear:hover {
+    background: rgba(255, 150, 113, 0.5);
+    }
+
+
     /* 調整月份的字體大小 */
     .month {
         color: white;
         font-size: 30px;
-        background-color: lightblue;
-        /* background: rgba(255, 150, 113, 0.5); */
+        /* background-color: lightblue; */
+        background: rgba(255, 150, 113, 0.5);
         border-radius: 20px;
         padding: 10px 20px;
         font-weight: bold;
@@ -148,172 +160,18 @@
 
     .year {
         font-size: 36px; 
-        color: #999; 
+        color: lightgray; 
         font-weight: bold; 
-        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);"
+        text-shadow: 2px 1px 1px rgba(0, 0, 0, 0.5);
     }
     
   </style>
 </head>
 <body>
-<h1>⁎ ⁂ ⁎ ⁑ * 萬年曆 * ⁑ ⁎ ⁂ ⁎</h1>   
-
-<?php
-/*請在這裹撰寫你的萬年曆程式碼*/  
-if(isset($_GET['month'])){
-  $month=$_GET['month']; // 如果有指定月份，使用它
-}else{
-  $month=date("m"); // 否則使用當前月份
-}
-
-if (isset($_GET['year'])) {
-  $year = $_GET['year'];
-} else {
-  $year = date("Y");
-}
-
-// 計算前一個月和前一年的月份
-if($month-1<1){
-  $prevMonth=12;
-  $prevYear=$year-1;
-}else{
-  $prevMonth=$month-1;
-  $prevYear=$year;
-}
-
-// 計算下一個月和後一年的月份
-if($month+1>12){
-  $nextMonth=1;
-  $nextYear=$year+1;
-}else{
-  $nextMonth=$month+1;
-  $nextYear=$year;
-}
-
-// 計算前一年與後一年
-$prevYearMonth = $year - 1;
-$nextYearMonth = $year + 1;
-
-$spDate=[
-'2024-11-07'=>"立冬",
-'2024-06-10' => "端午節",
-'2024-09-17' => "中秋節",
-'2025-05-31' => "端午節",
-'2025-10-06' => "中秋節",
-'2026-06-19' => "端午節",
-'2026-09-25' => "中秋節",
-'2024-11-22'=>"小雪"
-];
-
-$holidays = [
-'01-01' => "元旦",
-'02-10' => "農曆新年",
-'04-04' => "兒童節",
-'04-05' => "清明節",
-'05-01' => "勞動節",
-'10-10' => "國慶日",
-];
-
-?>
-<div class='nav'>
-  <table style="width:100%">
-      <tr>
-          <td style='text-align:left'>
-              <!-- <a href="calendar.php?year=<?=$year-2;?>">前年</a> -->
-              <a href="calendar.php?year=<?=$prevYearMonth;?>&month=<?=$month;?>"><<</a>
-              <br>
-              <a href="calendar.php?year=<?=$prevYear;?>&month=<?=$prevMonth;?>"><</a>
-          </td>
-          <td>
-              <?php echo "{$year}年 <br> {$month}月";?>
-          </td>
-          <td style='text-align:right'>
-              <a href="calendar.php?year=<?=$nextYearMonth;?>&month=<?=$month;?>">>></a>
-              <br>
-              <a href="calendar.php?year=<?=$nextYear;?>&month=<?=$nextMonth;?>">></a>
-              <!-- <a href="calendar.php?year=<?=$year+1;?>">明年</a> -->      
-          </td>
-      </tr>
-  </table>
-</div>
-
-<!-- 「今天」按鈕 -->
-<!-- 設定按鈕的連結，點擊後導向當前月份的日曆頁面 -->
-<div class="today-button">
-  <a href="calendar.php?year=<?php echo date('Y'); ?>&month=<?php echo date('m'); ?>" class="today-link">
-      今天
-  </a>
-</div>
-
-<table>
-  <tr>
-    <!-- <td></td> -->
-    <th>SUN &nbsp; 日</th>
-    <th>MON &nbsp; 一</th>
-    <th>TUE &nbsp; 二</th>
-    <th>WED &nbsp; 三</th>
-    <th>THU &nbsp; 四</th>
-    <th>FRI &nbsp; 五</th>
-    <th>SAT &nbsp; 六</th>
-  </tr>
-
-<?php
-
-// 計算當月的第一天
-$firstDay="{$year}-{$month}-1"; 
-/* $firstDay=date("Y-m-1"); */
-$firstDayTime=strtotime($firstDay); // 將第一天轉換成時間戳
-$firstDayWeek=date("w",$firstDayTime); // 獲取第一天是星期幾
-
-// 逐行顯示每一天
-for($i=0;$i<6;$i++){
-  echo "<tr>"; 
-  // echo "<td>";
-  // echo $i+1; //顯示週數
-  // echo "</td>";
-  for($j=0;$j<7;$j++){
-      //echo "<td class='holiday'>";
-      // 計算這個格子中的日期
-      $cell=$i*7+$j -$firstDayWeek;
-      $theDayTime=strtotime("$cell days".$firstDay);
-
-      //所需樣式css判斷（假日、非當月等）
-      $theMonth=(date("m",$theDayTime)==date("m",$firstDayTime))?'':'grey-text';
-      $isToday=(date("Y-m-d",$theDayTime)==date("Y-m-d"))?'today':'';
-      $w=date("w",$theDayTime);
-      $isHoliday=($w==0 || $w==6)?'holiday':''; // 假日（星期六和星期天）
-      
-      //顯示日期
-      echo "<td class='$isHoliday $theMonth $isToday'>";
-      echo date("d",$theDayTime); 
-
-      //如果有特定日期程式撰寫
-      if(isset($spDate[date("Y-m-d",$theDayTime)])){
-          echo "<br><span class='sp-date'>{$spDate[date("Y-m-d",$theDayTime)]}</span>";
-      }
-
-      //國定假日程式撰寫(如果想要改成不同顏色，要再上面新增CSS判斷)
-      //目前是農曆的節日要再另外設計
-      if(isset($holidays[date("m-d",$theDayTime)])){
-          echo "<br><span class='holiday-text'>{$holidays[date("m-d",$theDayTime)]}</span>";
-      }
-
-
-      echo "</td>";
-      
-  }
-  echo "</tr>"; // 結束一行
-}
-?>
-</table>
-
-<br>
-<br>
-<br>
-<br>
-
-<h1>⁎ ⁂ ⁎ ⁑ * 萬年曆 * ⁑ ⁎ ⁂ ⁎</h1>   
-
+<h1>.⁎ .⁂ .⁎˙ ⁑ * 萬  年  曆 * ⁑ ˙⁎. ⁂. ⁎.  </h1> 
+ <!-- <h2> (๑◕ܫ◕๑)ฅ . ฅ (๑•̀ ω •́๑) ฅ ʕ•͡ᴥ•ʔ ✿●  </h2> -->
+<br>  
+<br> 
 <?php
 /*請在這裹撰寫你的萬年曆程式碼*/  
 if(isset($_GET['month'])){
@@ -379,27 +237,27 @@ $holidays = [
                 <?php echo " {$month}月";?>
             </td>      
             <td class="nextYear">
-                <a href="calendar.php?year=<?=$prevYearMonth;?>&month=<?=$month;?>">◄◄</a>                              
+                <a href="index.php?year=<?=$prevYearMonth;?>&month=<?=$month;?>">◄◄</a>                              
             </td>
             <td class="year" style="font-size: 36px;">
                 <?php echo "{$year}年" ;?>
             </td>
             <td class="nextYear">
-                <a href="calendar.php?year=<?=$nextYearMonth;?>&month=<?=$month;?>">►►</a>    
+                <a href="index.php?year=<?=$nextYearMonth;?>&month=<?=$month;?>">►►</a>    
             </td>
         </tr>
 
         <tr>
-        <td style='text-align:left'>
-            <a href="calendar.php?year=<?=$prevYear;?>&month=<?=$prevMonth;?>"><</a>
+        <td class="nextMonth" style='text-align:left'>
+            <a href="index.php?year=<?=$prevYear;?>&month=<?=$prevMonth;?>">◄</a>
         </td>
         <td class="today-button">
-            <a href="calendar.php?year=<?php echo date('Y'); ?>&month=<?php echo date('m'); ?>" class="today-link1">
-            今天
+            <a href="index.php?year=<?php echo date('Y'); ?>&month=<?php echo date('m'); ?>" class="today-link">
+            T♥DAY
             </a>        
         </td>
-        <td style='text-align:right'>
-            <a href="calendar.php?year=<?=$nextYear;?>&month=<?=$nextMonth;?>">></a>
+        <td class="nextMonth" style='text-align:right'>
+            <a href="index.php?year=<?=$nextYear;?>&month=<?=$nextMonth;?>">►</a>
         </td>
     </tr>
 
@@ -409,13 +267,13 @@ $holidays = [
 <table>
 <tr>
     <!-- <td></td> -->
-    <th>SUN &nbsp; 日</th>
+    <th style='color:red'>✿ SUN 日</th>
     <th>MON &nbsp; 一</th>
     <th>TUE &nbsp; 二</th>
     <th>WED &nbsp; 三</th>
     <th>THU &nbsp; 四</th>
     <th>FRI &nbsp; 五</th>
-    <th>SAT &nbsp; 六</th>
+    <th style='color:red'>SAT 六 ✿</th>
 </tr>
 
 <?php
@@ -458,8 +316,6 @@ for($i=0;$i<6;$i++){
         if(isset($holidays[date("m-d",$theDayTime)])){
             echo "<br><span class='holiday-text'>{$holidays[date("m-d",$theDayTime)]}</span>";
         }
-
-
         echo "</td>";
         
     }
@@ -469,8 +325,6 @@ for($i=0;$i<6;$i++){
 ?>
 
 </table>
-
-
   
 </body>
 </html>
